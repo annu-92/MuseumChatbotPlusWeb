61a64e57c8cc5916e33a86e56107d5a8da345931
annu-92 <141201041+annu-92@users.noreply.github.com>
2025-01-13 21:25:06 +0530

    password field updated

---
+++ auth.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-app.js";
+import {  getAuth } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-auth.js";
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyDfzN32_IgM-0fqZ3Yle_Rc7LL03K90m-s",
+    authDomain: "signup-af1c6.firebaseapp.com",
+    projectId: "signup-af1c6",
+    storageBucket: "signup-af1c6.appspot.com",
+    messagingSenderId: "101468690108",
+    appId: "1:101468690108:web:ea5e40a2579b7b02ec22b9"
+  };
+
+  
+  const app = initializeApp(firebaseConfig);
